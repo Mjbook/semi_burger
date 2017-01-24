9df122e41227b8ee80f74e5ecc506357851f1a8d
semi_burger <semi_burger@DESKTOP-OJ0F2F6>
2017-01-24 09:48:13 +0900

    고객소리 DAO DTO

---
+++ semi_burger/src/board/board/notice/QA/QADAO.java
@@ -0,0 +1,24 @@
+package board.board.notice.QA;
+
+import java.sql.*;
+
+public class QADAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	
+	public int getMaxRef() {
+		try {
+			
+		}catch(Exception e) {
+			
+		}finally {
+			try {
+				
+			}catch(Exception e) {}
+		}
+	}
+	
+
+}
